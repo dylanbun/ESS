af2c5db67814899203cef7065f463e2d87b0a3c1
dylanbun <56641275+dylanbun@users.noreply.github.com>
2020-11-26 20:50:42 -0800

    Update App.js
    Added remove input function just for user's input. Also updated login page.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
-import firebase, {auth, provider} from './firebase.js';
+import firebase, {auth, provider, storage} from './firebase.js';
 
 // Run with this using 'npm start' !!!
 // To do list:
-// Need to add ability for user to only remove their input
+// Placement of boxes when displaying items
+// Need to add ability to submit photos + be seen by users
+// Dropdown menu for essential supplies
+// Notification system?
 class App extends Component {
   constructor() {
     super();
@@ -46,7 +49,7 @@ class App extends Component {
     const itemsRef = firebase.database().ref('items');
     const item = {
       title: this.state.currentItem,
-      user: this.state.username
+      user: this.state.user.displayName || this.state.user.email
     }
     itemsRef.push(item);
     this.setState({
@@ -82,58 +85,61 @@ class App extends Component {
   }
   render() {
     return (
-      // Header makes login/logout button and title
-      // last </div> is at bottom at line 110
       <div className='app'>
         <header>
-            <div className="wrapper">
-              <h1>Essential Supplies Stock</h1>
-              {this.state.user ?
-                <button onClick = {this.logout}>Log Out</button>
-                :
-                <button onClick = {this.login}>Log In</button>
-              }            
-            </div>
+          <div className="wrapper">
+            <h1>Essential Supplies Stock</h1>
+            {this.state.user ?
+              <button onClick={this.logout}>Logout</button>                
+            :
+              <button onClick={this.login}>Log In</button>              
+            }
+          </div>
         </header>
-        {this.state.user ? 
+        {this.state.user ?
           <div>
-            <div className = 'user-profile'>
-              <img src = {this.state.user.photoURL}/>
+            <div className='user-profile'>
+              <img src={this.state.user.photoURL} />
             </div>
-          </div>
-          :
-          <div className = 'wrapper'>
-            <p> You must be logged in to see the stock!</p>
-          </div>
-        }
-        <div className='container'>
-          <section className='add-item'>
+            <div className='container'>
+              {/* .. */}
+              <section className='display-item'>
+                <div className="wrapper">
+                  <ul>
+                    {this.state.items.map((item) => {
+                      return (
+                        <li key={item.id}>
+                          <h3>{item.title}</h3>
+                          <p>Product: {item.user}
+                            {item.user === this.state.user.displayName || item.user === this.state.user.email ?
+                              <button onClick={() => this.removeItem(item.id)}>Remove Item</button> : null}
+                          </p>
+                        </li>
+                      )
+                    })}
+                  </ul>
+                </div>
+              </section>
+            </div>
+            <div className='container'>
+              <section className='add-item'>
                 <form onSubmit={this.handleSubmit}>
-                  <input type="text" name="username" placeholder="What's your name?" onChange={this.handleChange} value={this.state.username} />
+                  <input type="text" name="username" placeholder="What's your name?" value={this.state.user.displayName || this.state.user.email} />
                   <input type="text" name="currentItem" placeholder="What are you bringing?" onChange={this.handleChange} value={this.state.currentItem} />
                   <button>Add Item</button>
                 </form>
-          </section>
-          <section className='display-item'>
-              <div className="wrapper">
-                <ul>
-                  {this.state.items.map((item) => {
-                    return (
-                      <li key={item.id}>
-                        <h3>{item.title}</h3>
-                        <p>From: <strong>{item.user}</strong>
-                          <button onClick={() => this.removeItem(item.id)}>Remove Item</button>
-                        </p>
-                      </li>
-                    )
-                  })}
-                </ul>
-              </div>
-          </section>
-        </div>
+              </section>
+            </div>
+          </div>
+          :
+          <div className='wrapper'>
+            <p> <h2> You must be logged in to see the stock.</h2></p>
+          </div>
+        }
       </div>
     );
   }
+  
 }
 
 export default App;
