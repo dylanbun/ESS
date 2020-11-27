05da565b0ed7775c4e820c6d87cb84297bc59180
dylanbun <56641275+dylanbun@users.noreply.github.com>
2020-11-26 20:51:35 -0800

    Update firebase.js
    Added storage function for future implementation.

--- src/firebase.js
+++ src/firebase.js
@@ -1,19 +1,26 @@
 import firebase from 'firebase'
+import 'firebase/storage'
+<script src="https://www.gstatic.com/firebasejs/8.1.1/firebase-app.js"></script>
+
   // Your web app's Firebase configuration
   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
   var firebaseConfig = {
-    apiKey: "AIzaSyAiQcHBl-WbUMlf7OtUhFpgGG6elcVwq0Y",
-    authDomain: "essential-supplies-s.firebaseapp.com",
-    databaseURL: "https://essential-supplies-s.firebaseio.com",
-    projectId: "essential-supplies-s",
-    storageBucket: "essential-supplies-s.appspot.com",
-    messagingSenderId: "910417048349",
-    appId: "1:910417048349:web:aa6707e7d94f53893127f4",
-    measurementId: "G-60WZZKZK1T"
+    apiKey: "AIzaSyAUw6CV5-3w8J_qvHPaJz-P5NXazThc8MU",
+    authDomain: "essential-supplies-stock.firebaseapp.com",
+    databaseURL: "https://essential-supplies-stock.firebaseio.com",
+    projectId: "essential-supplies-stock",
+    storageBucket: "essential-supplies-stock.appspot.com",
+    messagingSenderId: "335873225600",
+    appId: "1:335873225600:web:ebd20b1392e4e1a48cc765",
+    measurementId: "G-EH5ER8YD2X"
   };
+  <script src="https://www.gstatic.com/firebasejs/8.1.1/firebase-analytics.js"></script>
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
   firebase.analytics();
-export const provider = new firebase.auth.GoogleAuthProvider();
-export const auth = firebase.auth();
-export default firebase;
+  const provider = new firebase.auth.GoogleAuthProvider();
+  const auth = firebase.auth();
+  const storage = firebase.storage();
+  export {
+    storage, provider, auth, firebase as default
+  }
